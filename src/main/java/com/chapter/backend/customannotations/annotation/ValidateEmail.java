512cb1ee4dce5da3c8f3a087f089b346a366d2eb
Angela Boakes <angela@backbase.com>
2024-11-01 16:52:07 +0000

    fix

--- src/main/java/com/chapter/backend/customannotations/annotation/ValidateEmail.java
+++ src/main/java/com/chapter/backend/customannotations/annotation/ValidateEmail.java
@@ -15,4 +15,8 @@ public @interface ValidateEmail {
 
     String message() default "Invalid email address";
 
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
 }
